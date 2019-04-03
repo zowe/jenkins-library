65c76b64d3e33c5cb57784e669bdd408d6e82555
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-04-03 16:11:33 -0400

    try redefine transiient on sub class
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/artifact/JFrogArtifactory.groovy
+++ src/org/zowe/jenkins_shared_library/artifact/JFrogArtifactory.groovy
@@ -10,6 +10,7 @@
 
 package org.zowe.jenkins_shared_library.artifact
 
+import com.cloudbees.groovy.cps.NonCPS
 import org.zowe.jenkins_shared_library.exceptions.InvalidArgumentException
 import org.zowe.jenkins_shared_library.exceptions.UnderConstructionException
 
@@ -17,6 +18,11 @@ import org.zowe.jenkins_shared_library.exceptions.UnderConstructionException
  * Operating artifacts with jFrog Artifatory CLI commands or API
  */
 class JFrogArtifactory extends ArtifactBase {
+    /**
+     * logger object to write logs
+     */
+    transient Logger logger
+
     /**
      * CLI config name
      */
