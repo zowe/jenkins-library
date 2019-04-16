78de48719016e3b76756558d1acb53fb1d884b9f
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-04-16 16:55:43 -0400

    more tests
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/ClassGrandchild.groovy
+++ src/org/zowe/jenkins_shared_library/ClassGrandchild.groovy
@@ -15,13 +15,38 @@ import java.util.logging.Level
 class ClassGrandchild extends ClassChild {
     def steps
 
+    void printAllMethods( obj ){
+        if( !obj ){
+            steps.echo "Object is null\r\n";
+            return;
+        }
+        if( !obj.metaClass && obj.getClass() ){
+            printAllMethods( obj.getClass() );
+            return;
+        }
+        def str = "class ${obj.getClass().name} functions:\r\n";
+        obj.metaClass.methods.name.unique().each{
+            str += it+"(); ";
+        }
+        steps.echo  "${str}\r\n";
+    }
+
+
     ClassGrandchild(steps) {
         super(steps)
-        steps.echo "ClassGrandchild construction ${super}"
+        steps.echo "ClassGrandchild construction"
+        printAllMethods(super)
+        steps.echo "super = ${super}"
+        steps.echo "super.getClass = ${super.getClass()}"
     }
 
     void test() {
-        steps.echo "ClassGrandchild.test() // ${super} // ${super.test}"
+        steps.echo "ClassGrandchild.test()"
+        printAllMethods(super)
+        steps.echo "super = ${super}"
+        steps.echo "super.getClass = ${super.getClass()}"
+        steps.echo "super.test = ${super.test}"
+        steps.echo "super.test.getClass = ${super.test.getClass()}"
         super.test()
         steps.echo "ClassGrandchild.test()"
     }
