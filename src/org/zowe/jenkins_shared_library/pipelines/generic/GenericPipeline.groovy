783191f4686cc319f2e5b5db8182ad1f3a041aeb
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-04-17 12:54:57 -0400

    import @Log
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
@@ -11,6 +11,7 @@
 package org.zowe.jenkins_shared_library.pipelines.generic
 
 // import com.cloudbees.groovy.cps.NonCPS
+import groovy.util.logging.Log
 import java.util.regex.Pattern
 import org.zowe.jenkins_shared_library.artifact.JFrogArtifactory
 import org.zowe.jenkins_shared_library.pipelines.base.enums.ResultEnum
