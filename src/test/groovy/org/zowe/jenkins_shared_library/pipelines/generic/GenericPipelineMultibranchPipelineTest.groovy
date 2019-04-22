73b6d3c8f42cea4b04b23faffc17d368a0d8486f
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-04-22 15:09:48 -0400

    what's the name?
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/test/groovy/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipelineMultibranchPipelineTest.groovy
+++ src/test/groovy/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipelineMultibranchPipelineTest.groovy
@@ -150,9 +150,10 @@ class GenericPipelineMultibranchPipelineTest extends IntegrationTest {
         def tags = HttpRequest.getJson(tagsUrl)
         List<String> tagNames = []
         tags.each {
+            logger.find("tag: ${it['name']} (${it['name'].getClass()})")
             tagNames.push(it['name'])
         }
-        def expectedTag = "v${currentVersion['major']}.${currentVersion['minor']}.${currentVersion['patch']}-${preReleaseString}"
+        String expectedTag = "v${currentVersion['major']}.${currentVersion['minor']}.${currentVersion['patch']}-${preReleaseString}"
         logger.fine("All tags: ${tagNames}")
         logger.fine("Expected tag: ${expectedTag}")
         // check if we have the tag
