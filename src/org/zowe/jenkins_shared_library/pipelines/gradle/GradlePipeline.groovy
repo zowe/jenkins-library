c734449d55bfc1e6837ac564a32ea2ce58a1c9c3
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-10-24 09:25:06 -0400

    missing definition
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/pipelines/gradle/GradlePipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/gradle/GradlePipeline.groovy
@@ -20,6 +20,7 @@ import org.zowe.jenkins_shared_library.pipelines.base.models.StageTimeout
 import org.zowe.jenkins_shared_library.pipelines.Build
 import org.zowe.jenkins_shared_library.pipelines.Constants
 import org.zowe.jenkins_shared_library.pipelines.generic.arguments.ReleaseStageArguments
+import org.zowe.jenkins_shared_library.pipelines.generic.arguments.SonarScanStageArguments
 import org.zowe.jenkins_shared_library.pipelines.generic.exceptions.*
 import org.zowe.jenkins_shared_library.pipelines.generic.GenericPipeline
 import org.zowe.jenkins_shared_library.pipelines.gradle.arguments.*
