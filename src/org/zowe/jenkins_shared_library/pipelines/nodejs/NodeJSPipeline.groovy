bf742449af9dab614ed3edea632221d018f913f0
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-03-10 15:24:48 -0400

    set +x to disable echo on pipeline
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/pipelines/nodejs/NodeJSPipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/nodejs/NodeJSPipeline.groovy
@@ -240,7 +240,7 @@ class NodeJSPipeline extends GenericPipeline {
      */
     String nvmShell(script) {
         if (this.nvmInitScript && this.nodeJsVersion) {
-            steps.sh ". ${this.nvmInitScript} >/dev/null && nvm use ${this.nodeJsVersion} && ${script}"
+            steps.sh "set +x && . ${this.nvmInitScript} >/dev/null && set -x && nvm use ${this.nodeJsVersion} && ${script}"
         } else {
             steps.sh script
         }
@@ -355,7 +355,7 @@ class NodeJSPipeline extends GenericPipeline {
                 pipeline.nodeJsVersion = arguments.nodeJsVersion
                 pipeline.nvmInitScript = arguments.nvmInitScript
                 pipeline.steps.echo "Pipeline will use node.js ${pipeline.nodeJsVersion} to build and test"
-                pipeline.steps.sh ". ${pipeline.nvmInitScript} >/dev/null && nvm install ${pipeline.nodeJsVersion}"
+                pipeline.steps.sh "set +x && . ${pipeline.nvmInitScript} >/dev/null && set -x && nvm install ${pipeline.nodeJsVersion}"
             }
         }
         // should we overwrite this?
