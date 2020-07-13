a5c568a98df1ff6dc1a7708661c5d9714818a27e
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-07-13 17:24:25 -0400

    Docker in need of sudo
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- src/org/zowe/jenkins_shared_library/package/Pax.groovy
+++ src/org/zowe/jenkins_shared_library/package/Pax.groovy
@@ -437,7 +437,7 @@ EOF"""
 get ${remoteWorkspaceFullPath}/${compressPax ? filePaxZ : filePax} ${this.localWorkspace}${extraGets}
 EOF"""
 
-                    this.steps.sh """docker build https://github.com/1000TurquoisePogs/zowe-dockerfiles.git#s390x:dockerfiles/zowe-release/amd64/zowe-v1-lts --build-arg PAX_FILE=${this.localWorkspace}/${filePax}"""
+                    this.steps.sh """sudo systemctl start docker && sudo docker build https://github.com/1000TurquoisePogs/zowe-dockerfiles.git#s390x:dockerfiles/zowe-release/amd64/zowe-v1-lts --build-arg PAX_FILE=${this.localWorkspace}/${filePax}"""
                 } catch (ex1) {
                     // throw error
                     throw new PackageException("Pack Pax package failed: ${ex1}")
