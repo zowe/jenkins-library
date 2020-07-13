6bbfd32137d84f9a28c1c43ca945c9783126e1c4
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-07-13 18:36:13 -0400

    No systemd
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- src/org/zowe/jenkins_shared_library/package/Pax.groovy
+++ src/org/zowe/jenkins_shared_library/package/Pax.groovy
@@ -437,7 +437,8 @@ EOF"""
 get ${remoteWorkspaceFullPath}/${compressPax ? filePaxZ : filePax} ${this.localWorkspace}${extraGets}
 EOF"""
 
-                    this.steps.sh """id && systemctl start docker && docker build https://github.com/1000TurquoisePogs/zowe-dockerfiles.git#s390x:dockerfiles/zowe-release/amd64/zowe-v1-lts --build-arg PAX_FILE=${this.localWorkspace}/${filePax}"""
+                    this.steps.sh """dockerd"""
+                    this.steps.sh """docker build https://github.com/1000TurquoisePogs/zowe-dockerfiles.git#s390x:dockerfiles/zowe-release/amd64/zowe-v1-lts --build-arg PAX_FILE=${this.localWorkspace}/${filePax}"""
                 } catch (ex1) {
                     // throw error
                     throw new PackageException("Pack Pax package failed: ${ex1}")
