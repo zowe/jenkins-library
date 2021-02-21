543b056b06cc0eac5134f3b229852bee9beb5cc0
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-02-21 12:09:30 -0500

    move authorized user check into stage
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
@@ -258,8 +258,6 @@ class GenericPipeline extends Pipeline {
      */
     protected GenericPipelineControl _control = new GenericPipelineControl()
 
-    boolean isAuthorizedUser
-
     /**
      * Constructs the class.
      *
@@ -1433,19 +1431,6 @@ class GenericPipeline extends Pipeline {
         //     preSetupException = new PackagingStageException("arguments.localWorkspace is not defined for packagingGeneric", arguments.name)
         // }
 
-        Integer causeID = this.build.getCause()
-        this.isAuthorizedUser = false
-        if (causeID == PipelineConstants.USERID_CAUSE_ID) {
-            this.isAuthorizedUser = true
-        } else if (causeID == PipelineConstants.BRANCHEVENT_CAUSE_ID || causeID == PipelineConstants.BRANCHINDEXING_CAUSE_ID) {
-            if (this.changeInfo.isPullRequest) {
-                this.isAuthorizedUser = isPRAuthorizedUser()
-            }
-        }
-        if (!this.isAuthorizedUser) {
-            preSetupException = new PackagingStageException("Automatic packaging step for non-committers on z/OS is disabled.", arguments.name)
-        }
-
         def originalPackageName = arguments.name
         // now arguments.name is used as stage name
         arguments.name = "Packaging: ${arguments.name}"
@@ -1455,6 +1440,26 @@ class GenericPipeline extends Pipeline {
             // can be sent.
             if (preSetupException) {
                 throw preSetupException
+            }
+
+            Integer causeID = this.build.getCause()
+            boolean proceedPaxPackaging = false
+            if (causeID == PipelineConstants.USERID_CAUSE_ID) {
+                // allow pax packaging if user has Jenkins access
+                proceedPaxPackaging = true
+            } else if (causeID == PipelineConstants.BRANCHEVENT_CAUSE_ID || causeID == PipelineConstants.BRANCHINDEXING_CAUSE_ID) {
+                if (this.changeInfo.isPullRequest) {
+                    // for pull-request build, if it's triggered by automation
+                    // only users with write permission to the repository can proceed
+                    proceedPaxPackaging = isPRAuthorizedUser()
+                } else {
+                    // if it's branch build, we let it go
+                    // the user should already have write permission to the repository
+                    proceedPaxPackaging = true
+                }
+            }
+            if (!proceedPaxPackaging) {
+                throw new PackagingStageException("Automatic packaging step for non-committers on z/OS is disabled.", arguments.name)
             }
 
             // execute operation Closure if provided
@@ -1538,7 +1543,7 @@ class GenericPipeline extends Pipeline {
         String prNumberString = "${this.changeInfo.pullRequestId}"   // this will be PR number
         int prNumber = prNumberString as Integer   // convert to int
         def user = this.github.getPullRequestUser(prNumber)
-        
+
         return this.github.isUserWriteCollaborator(user)
     }
 
