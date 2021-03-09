642e1247b8c3b57a55faa4481673abda0ee4bd9d
Tom Zhang <tom.yczhang@gmail.com>
2021-03-09 17:30:30 -0500

    Update Build.groovy
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- src/org/zowe/jenkins_shared_library/pipelines/Build.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/Build.groovy
@@ -194,13 +194,13 @@ class Build {
     private Integer identifyCause(Cause c) {
         log.finer("DEBUG Cause properties are: ${c.properties}")
         def causeID
-        if (c.properties.get("class").contains("BranchEventCause")) {
+        if (c.toString().contains("BranchEventCause")) {
             causeID = PipelineConstants.BRANCHEVENT_CAUSE_ID      //PR open or updated
         }
         else if (c instanceof hudson.model.Cause$UserIdCause) {
             causeID = PipelineConstants.USERID_CAUSE_ID           // jenkins user trigger
         }
-        else if (c.properties.get("class").contains("BranchIndexingCause")) {
+        else if (c.toString().contains("BranchIndexingCause")) {
             causeID = PipelineConstants.BRANCHINDEXING_CAUSE_ID   //'Scan Repository Now' action
         }
         else if (c instanceof hudson.model.Cause$RemoteCause) {
