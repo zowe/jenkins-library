186eea0bc234a0f5fe528650abb06f356f444df8
Tom Zhang <tom.yczhang@gmail.com>
2021-03-11 13:49:52 -0500

    Update GenericPipeline.groovy
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
@@ -1450,7 +1450,8 @@ class GenericPipeline extends Pipeline {
             // def contentString = "testcomment1\n\ttestcomment2\n\\backslash\\"
             // def contentString = "testcomment1\n\r\ttestcomment2\n\r\\backslash\\"
             // def contentString = "testcomment1\n\r\ttestcomment2\n\r\\backslash\\\n\$dollarsign\$\n"
-            def contentString = "testcomment1\n\r\ttestcomment2\n\r\\backslash\\\n\$dollarsign\$\n'single quotations'"
+            // def contentString = "testcomment1\n\r\ttestcomment2\n\r\\backslash\\\n\$dollarsign\$\n'single quotations'\"double quotations\""
+            def contentString = "testcomment1\n\r\ttestcomment2\n\r\\backslash\\\n\$dollarsign\$\n\"double quotations\""
             steps.echo "ESCAPE DDDDDDDEEEEEEEBBBBBBUUUUUUGGGGGGG: original: $contentString"
             // contentString = contentString.replaceAll("\\\\", "\\\\\\\\")    v
             //                  .replaceAll(/\r/, "\\\\r")   v
