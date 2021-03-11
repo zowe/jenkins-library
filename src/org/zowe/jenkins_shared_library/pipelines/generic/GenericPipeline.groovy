803a28380b37e45fc3b84b418765c54e20d6c60e
Tom Zhang <tom.yczhang@gmail.com>
2021-03-11 10:06:56 -0500

    test
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/jenkins_shared_library/pipelines/generic/GenericPipeline.groovy
@@ -1444,11 +1444,7 @@ class GenericPipeline extends Pipeline {
             //test for posting a comment
             String prNumberString = "${this.changeInfo.pullRequestId}"   // this will be PR number
             int prNumber = prNumberString as Integer   // convert to int
-            def contentString = "test comment\n" +
-                    '\tline with "double quotations"\n' +
-                    "\tline with 'single quotations'\n" +
-                    "\tline with \\backslash\\\n" +
-                    "\tline with \$dollar\$\n"
+            def contentString = "test comment\n\tline with \"double quotations\"\n\tline with \'single quotations\'\n\tline with \\backslash\\\n\tline with \$dollar\$\n"
             // contentString = contentString.replaceAll("\\\\", "\\\\\\\\")
             //                  .replaceAll(/\r/, "\\\\r")
             //                  .replaceAll(/\n/, "\\\\n")
